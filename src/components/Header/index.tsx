fa2c548a79213966e91456a12f464f8d31e10d07
ThiagoPederzolli <thiagopederzolli@gmail.com>
2021-03-27 13:05:24 -0300

    concluido desafio chapter II

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
 
 import logo from '../../assets/images/logo.svg';
 import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
+import { Product } from '../../types';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+  const [cartSize, setCartSize] = useState(0);
+
+  useEffect(() => {
+    const updateCartSize = cart.reduce(
+      (acc, product) => {
+        const haveProduct = acc.find((productCart: Product) => productCart.id === product.id);
+      if (!haveProduct) return [...acc, product]
+      return acc;
+    }, [] as Product[]).length;
+    setCartSize(updateCartSize);
+  }, [cart])
+  
 
   return (
     <Container>
@@ -20,7 +32,7 @@ const Header = (): JSX.Element => {
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,28 +23,56 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
-
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
-
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const haveProductInCart = cart.find((cartProductId) => cartProductId.id === productId);
+      const {data: stock} = await api.get<Stock>(`stock/${productId}`)
+      
+      if (!haveProductInCart) {
+        const {data: product} = await api.get<Product>(`products/${productId}`);
+        const addProductInCart = {...product, amount: 1}
+        if (addProductInCart.amount <= stock.amount) {
+          setCart([...cart, addProductInCart]);
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify([...cart, addProductInCart]))
+        } else {
+          toast.error('Quantidade solicitada fora de estoque');
+        }
+      } else {
+        if (haveProductInCart.amount < stock.amount) {
+          const amountOfProduct = haveProductInCart.amount;
+          await setCart([...cart.filter((cartId) => cartId.id !== haveProductInCart.id), {...haveProductInCart, amount: amountOfProduct + 1}]);
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify([...cart.filter((cartId) => cartId.id !== haveProductInCart.id), {...haveProductInCart, amount: amountOfProduct + 1}]))
+        } else {
+          toast.error('Quantidade solicitada fora de estoque');
+        }
+      }
     } catch {
       // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
       // TODO
+      const removingProduct = cart.find((product: Product) => product.id ===  productId)
+        if (!removingProduct) {
+          toast.error('Erro na remoção do produto');
+          return;
+        } else {
+          setCart([...cart.filter((cartId) => cartId.id !== removingProduct.id)]);
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify([...cart.filter((cartId) => cartId.id !== removingProduct.id)]))
+        }
     } catch {
       // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
@@ -53,9 +81,25 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
+      const {data: stock} = await api.get<Stock>(`stock/${productId}`)
+
       // TODO
+      const updateAmount = cart.find((product: Product) => product.id ===  productId)
+      if (updateAmount && amount < 1) {        
+        return;
+      }
+      if (amount > stock.amount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return
+      }
+      if (updateAmount) {
+        setCart([...cart.filter((cartId) => cartId.id !== updateAmount.id), {...updateAmount, amount}]);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify([...cart.filter((cartId) => cartId.id !== updateAmount.id), {...updateAmount, amount}]))
+      }
+      
     } catch {
       // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
 
@@ -73,3 +117,21 @@ export function useCart(): CartContextData {
 
   return context;
 }
+
+
+    // }
+      // const addingProduct = response.data.find((product: Product) => product.id ===  productId)
+      // const haveProductInCart = cart.find((cartProductId) => cartProductId.id === addingProduct.id);
+      // if (!haveProductInCart) {
+      //   setCart([...cart, {...addingProduct, amount: 1}]);
+      //   localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
+      // } else {
+      //   const productInStock = stock.find((stockId) => stockId.id === productId);
+      //   if (productInStock &&  haveProductInCart.amount >= productInStock?.amount) {
+      //     toast.error('Quantidade solicitada fora de estoque');
+      //     return;
+      //   } else {
+      //     const amountOfProduct = haveProductInCart.amount;
+      //     setCart([...cart.filter((cartId) => cartId.id !== addingProduct.id), {...addingProduct, amount: amountOfProduct + 1}]);
+      //     localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
+      //   }
